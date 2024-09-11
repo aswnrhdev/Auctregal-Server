ec821083e7449e097d6e6bfc2cb4a67458a6db54
Aswin Ramesh <>
2024-09-11 12:02:51 +0530

    cors updated

--- index.ts
+++ index.ts
@@ -12,7 +12,11 @@ import ItemModel from "./src/models/itemModel";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true,
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 
